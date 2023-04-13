da28e7676ce7f2fa705edab9a3cf74ab90a0b937
Allan Bifom (aeb672) <aeb672@usask.ca>
2023-04-12 21:13:03 -0600

    Making a java Calculator

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Main extends JFrame implements ActionListener {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
